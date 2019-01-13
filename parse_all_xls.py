47b6d503945b9cebd15968c2b6ab571f1c8a0099
Matteo Ferla <matteo.ferla@gmail.com>
2019-01-13 21:38:56 +0000

    :bug: parse .DS_store? No thanks

--- parse_all_xls.py
+++ parse_all_xls.py
@@ -12,29 +12,35 @@ NP2NC.debugprint = print
 
 import pandas, os, pickle, csv
 
-
-out=csv.DictWriter(open('out.csv','w',newline='\n'),extrasaction='ignore',fieldnames=('file','sheet','row',
+out=csv.DictWriter(open(folder+'.csv','w',newline='\n'), extrasaction='ignore',fieldnames=('file','sheet','row',
                                                             'Protein name','Organism','AA sequence',
                                                             'matched_protein_acc','matched_prot_seq','matched_organism',
                                                             'protein_ID','gene_ID',
                                                             'genome_ID','genome_from','genome_to','symbol','locus'))
-
 out.writeheader()
 
 for file in os.listdir(folder):
+    if os.path.splitext(file)[-1].find('.xls') == -1: # DS Store or something
+        continue
+    print(file)
     sheets = pandas.read_excel(os.path.join(folder,file),sheet_name=None)
     for pi in sheets:
         for ri, row in sheets[pi].iterrows():
             print(row)
             try:
                 for ci in range(len(row)):
-                    if len(row[ci]) > 100:
-                        seq = row[ci].replace('\n', '').replace('\r', '')
-                        entry={'file':file,'sheet': pi, 'row': ri, **NP2NC.fetch_identifier(sequence=seq),**dict(row)}
+                    if len(row[ci]) > 50:
+                        picklename=os.path.join(folder,file+'.'+str(pi)+'.'+str(ri)+'.p')
+                        if os.path.isfile(picklename):
+                            entry=pickle.load(open(picklename,'rb'))
+                        else:
+                            seq = row[ci].replace('\n', '').replace('\r', '')
+                            entry={'file':file,'sheet': pi, 'row': ri, **NP2NC.fetch_identifier(sequence=seq),**dict(row)}
+                            pickle.dump(entry,open(picklename,'wb'))
                         out.writerow(entry)
-                        pickle.dump(entry,open(file+'.'+str(pi)+'.'+str(ri)+'.p','wb'))
+                        break
+                    else:
+                        print('This row is problematic')
+                        print(row)
             except:
                 out.writerow({'file':file,'sheet': pi, 'row': ri})
-
-
-
